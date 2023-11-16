504d21c101930c1981fc68e85f5a962bd5eafd08
Mateusz MG <MateuszMG.default@com.com>
2023-11-16 07:33:58 +0100

    add icons

--- client/src/components/icons/Icons.tsx
+++ client/src/components/icons/Icons.tsx
@@ -1,5 +1,7 @@
-import { MdCancel } from 'react-icons/md';
+import { MdCancel, MdDelete, MdEdit } from 'react-icons/md';
 
 import styles from './Icon.module.css';
 
 export const CancelIcon = () => <MdCancel className={styles.icon} />;
+export const UpdateIcon = () => <MdEdit className={styles.icon} />;
+export const DeleteIcon = () => <MdDelete className={styles.icon} />;
