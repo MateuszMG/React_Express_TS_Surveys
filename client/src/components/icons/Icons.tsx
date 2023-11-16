b05f110090fe8386bd28119ef6f811ad1a870509
Mateusz MG <MateuszMG.default@com.com>
2023-11-16 07:46:22 +0100

    add Icon props

--- client/src/components/icons/Icons.tsx
+++ client/src/components/icons/Icons.tsx
@@ -1,7 +1,16 @@
+import { IconBaseProps } from 'react-icons';
 import { MdCancel, MdDelete, MdEdit } from 'react-icons/md';
 
 import styles from './Icon.module.css';
 
-export const CancelIcon = () => <MdCancel className={styles.icon} />;
-export const UpdateIcon = () => <MdEdit className={styles.icon} />;
-export const DeleteIcon = () => <MdDelete className={styles.icon} />;
+interface IconProps extends IconBaseProps {}
+
+export const CancelIcon = (props: IconProps) => (
+  <MdCancel className={styles.icon} {...props} />
+);
+export const UpdateIcon = (props: IconProps) => (
+  <MdEdit className={styles.icon} {...props} />
+);
+export const DeleteIcon = (props: IconProps) => (
+  <MdDelete className={styles.icon} {...props} />
+);
