82d718733136e3840c4f1b463ee179308c84a815
Mateusz MG <MateuszMG.default@com.com>
2023-11-17 15:28:51 +0100

    correct surveyApi

--- client/src/api/surveysApi.ts
+++ client/src/api/surveysApi.ts
@@ -1,8 +1,11 @@
 import { axios } from './axios';
 
 export const surveyApi = {
-  getSurveys: axios.get('/surveys'),
-  createSurvey: axios.post('/surveys'),
+  getSurveys: () => axios.get('/surveys'),
+
+  createSurvey: (data: Omit<Survey, 'id'>) => axios.post('/surveys', data),
+
   updateSurvey: (id: string) => axios.put(`/surveys/${id}`),
+
   deleteSurvey: (id: string) => axios.delete(`/surveys/${id}}`),
 };
