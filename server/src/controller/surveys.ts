faed790323ed4660ce1bb374a9450e320dacec09
Mateusz MG <MateuszMG.default@com.com>
2023-11-17 15:25:11 +0100

    add surveysController

--- server/src/controller/surveys.ts
+++ server/src/controller/surveys.ts
@@ -1,8 +1,47 @@
 import { Request, Response } from 'express';
+import { surveyModel } from '../models/survey';
 
 export const surveysController = {
-  getSurveys: async (req: Request, res: Response) => {},
-  createSurvey: async (req: Request, res: Response) => {},
-  updateSurvey: async (req: Request, res: Response) => {},
-  deleteSurvey: async (req: Request, res: Response) => {},
+  get: async (req: Request, res: Response) => {
+    try {
+      const surveys = surveyModel.get();
+
+      return res.json({ surveys });
+    } catch (error) {
+      return error;
+    }
+  },
+
+  create: async (req: Request, res: Response) => {
+    try {
+      surveyModel.create(req.body);
+
+      return res.sendStatus(201);
+    } catch (error) {
+      return error;
+    }
+  },
+
+  update: async (req: Request, res: Response) => {
+    try {
+      surveyModel.update({
+        id: req.params.id,
+        ...req.body,
+      });
+
+      return res.sendStatus(200);
+    } catch (error) {
+      return error;
+    }
+  },
+
+  delete: async (req: Request, res: Response) => {
+    try {
+      surveyModel.delete(req.params.id);
+
+      return res.sendStatus(204);
+    } catch (error) {
+      return error;
+    }
+  },
 };
